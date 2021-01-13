7c447fe0f56608814deaddbf75d6264d5f3429cc
kaly <kalimamadou@gmail.com>
2021-01-12 20:32:45 -0800

    added watermarked on pdf file using mergepage function

--- pdf-playground/pdf.py
+++ pdf-playground/pdf.py
@@ -15,13 +15,26 @@ import sys
 # How to combine pdf to a superpdf from the command line
 
 
-inputs = sys.argv[1:] # capture everything after py filename.py
+# inputs = sys.argv[1:] # capture everything after py filename.py
 
-def pdf_combiner(pdf_list):
-    merger = PyPDF2.PdfFileMerger()
-    for pdf in pdf_list:
-        print(pdf)
-        merger.append(pdf)
-    merger.write('super.pdf')
+# def pdf_combiner(pdf_list):
+#     merger = PyPDF2.PdfFileMerger()
+#     for pdf in pdf_list:
+#         print(pdf)
+#         merger.append(pdf)
+#     merger.write('super.pdf')
 
-pdf_combiner(inputs)
+# pdf_combiner(inputs)
+
+ # add WaterMarker
+template = PyPDF2.PdfFileReader(open('super.pdf', 'rb'))
+watermark = PyPDF2.PdfFileReader(open('wtr.pdf', 'rb'))
+output = PyPDF2.PdfFileWriter()
+
+for i in range(template.getNumPages()):
+    page = template.getPage(i)
+    page.mergePage(watermark.getPage(0))
+    output.addPage(page)
+
+    with open('watermarked_output.pdf', 'wb') as file:
+        output.write(file)
